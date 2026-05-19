90c018de4c144265d2b47a80636b08f2f38f8c0c
Josiah Elias <josiahelias11@gmail.com>
2026-05-19 12:46:04 +0100

    feat: setting environment variables

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://kqpqaocoougwxufbhfzs.supabase.co"; //import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = "sb_publishable_z3Evy7U0obB6UFX0m80ppw_dLQRjqdq"; //import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const supabaseUrl =  import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey =  import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
